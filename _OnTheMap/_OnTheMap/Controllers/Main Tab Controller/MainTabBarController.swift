38d7957f83ff51281e40d1b19d8aba3fc7deac7b
Suresh N <forsuresh@yandex.com>
2019-02-01 17:37:21 -0500

    Added Most of the Assets we need Added views to TabController behind login window 	- MapViewController 		- Added MMKMapView to Map 	- ListViewController (for now, it's a generic VC)

--- _OnTheMap/_OnTheMap/Controllers/Main Tab Controller/MainTabBarController.swift
+++ _OnTheMap/_OnTheMap/Controllers/Main Tab Controller/MainTabBarController.swift
@@ -13,6 +13,23 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .yellow
+        
+        let mapIcon = UITabBarItem(title: "MAP", image: #imageLiteral(resourceName: "icon_mapview-selected"), selectedImage: #imageLiteral(resourceName: "icon_mapview-deselected"))
+        let listIcon = UITabBarItem(title: "LIST", image: #imageLiteral(resourceName: "icon_listview-selected"), selectedImage: #imageLiteral(resourceName: "icon_listview-deselected"))
+        
+        let mapController = MapController()
+        let listController = File2()
+        
+        mapController.tabBarItem = mapIcon
+        listController.tabBarItem = listIcon
+        
+        let controllers = [mapController, listController]
+        
+        viewControllers = controllers
+        
+        
+        
+        
     }
     
     
---
+++ _OnTheMap/_OnTheMap/Controllers/Main Tab Controller/MapController.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  _OnTheMap
+//
+//  Created by admin on 2/1/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapController:UIViewController{
+    
+    var mapView: MKMapView = {
+       var mapView = MKMapView()
+        
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        return mapView
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.addSubview(mapView)
+        
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            ])
+        
+        
+    }
+    
+    
+}
