c23941d6d2a07442b3c07e7d27b62e1ecebfefb4
Suresh N <forsuresh@yandex.com>
2019-02-10 22:38:47 -0500

    2 functions in AddLocationController to determine validity of location & url fields respectively
    Added:
    AddLocationController:
    	* func isStringToURLValid
    	* func isStringToLocationValid
    	* All though both functions work.  For now there is no logic implemented in how to determine returning after the first failure instead of accidentally moving onto adding to Parse API Backend

--- _OnTheMap/_OnTheMap/Controllers/AddLocationController.swift
+++ _OnTheMap/_OnTheMap/Controllers/AddLocationController.swift
@@ -15,16 +15,19 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
     let customUIHeightSize: CGFloat = 55
     let cornerRadiusSize: CGFloat = 5
     
+    var globalLocation = CLLocation()
+    
     lazy var locationTextField: UITextField = {
         let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.clearsOnBeginEditing = true
         textField.defaultTextAttributes = black25textAttributes
+        textField.clearButtonMode = .whileEditing
         textField.attributedText = NSMutableAttributedString(string: "Enter a Location", attributes: grey25textAttributes)
         textField.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
         return textField
     }()
-
+    
     lazy var urlTextField: UITextField = {
         let textField = UITextField()
         textField.borderStyle = .roundedRect
@@ -34,11 +37,11 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
         textField.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
         return textField
     }()
-
+    
     lazy var findLocationButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = UIColor.steelBlue
-//        button.setAttributedTitle(NSAttributedString(string: "FIND LOCATION", attributes: white25textAttributes), for: .normal)
+        //        button.setAttributedTitle(NSAttributedString(string: "FIND LOCATION", attributes: white25textAttributes), for: .normal)
         button.setTitle("FIND LOCATION", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
         button.addTarget(self, action: #selector(handleFindLocation), for: .touchUpInside)
@@ -48,8 +51,21 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
         return button
     }()
     
+    lazy var deletePLISTButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.orange
+        //        button.setAttributedTitle(NSAttributedString(string: "delete PLIST", attributes: white25textAttributes), for: .normal)
+        button.setTitle("DELETE PLIST", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.addTarget(self, action: #selector(handleDeletePLIST), for: .touchUpInside)
+        button.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
+        button.layer.cornerRadius = cornerRadiusSize
+        button.clipsToBounds = true
+        return button
+    }()
+    
     let locationImageView: UIImageView = {
-       let imageView = UIImageView(image: #imageLiteral(resourceName: "icon_world"))
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "icon_world"))
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
@@ -59,11 +75,12 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
     
     
     override func viewDidLoad() {
-        
         view.backgroundColor = UIColor.white
+        urlTextField.delegate = self
+        locationTextField.delegate = self
         
         let stackView: UIStackView = {
-           let stack = UIStackView()
+            let stack = UIStackView()
             stack.axis = .vertical
             stack.alignment = .center
             stack.distribution = .fill
@@ -72,21 +89,19 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
             return stack
         }()
         
-        [locationTextField, urlTextField, findLocationButton].forEach{stackView.addArrangedSubview($0)}
+        [locationTextField, urlTextField, findLocationButton, deletePLISTButton].forEach{stackView.addArrangedSubview($0)}
         [stackView, locationImageView].forEach{view.addSubview($0)}
         
         
         NSLayoutConstraint.activate([
             locationImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
             locationImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            
-            
-            
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.topAnchor.constraint(equalTo: locationImageView.bottomAnchor, constant: 50),
             locationTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7),
             urlTextField.widthAnchor.constraint(equalTo: locationTextField.widthAnchor),
-            findLocationButton.widthAnchor.constraint(equalTo: locationTextField.widthAnchor)
+            findLocationButton.widthAnchor.constraint(equalTo: locationTextField.widthAnchor),
+            deletePLISTButton.widthAnchor.constraint(equalTo: locationTextField.widthAnchor)
             ])
     }
     
@@ -94,17 +109,68 @@ class AddLocationController: UIViewController, MKMapViewDelegate, UITextFieldDel
     
     //MARK:- Handlers
     @objc func handleFindLocation(){
-        print("Button Pressed")
+        isStringToLocationValid(testString: locationTextField.text ?? "", completion: handleIsStringToLocationValid(success:error:))
+        _ = isStringToURLValid(testString: urlTextField.text ?? "")
+        
+    }
+    
+    func isStringToURLValid(testString: String)-> String?{
+        if testString._isValidURL {
+            return testString._prependHTTPifNeeded()
+        } else {
+            let alertController = UIAlertController(title: "Invalid Entry", message: "Unable to convert website entry to valid URL", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true)
+            return nil
+        }
+    }
+    
+    func isStringToLocationValid(testString: String, completion: @escaping (Bool, Error?)-> Void){
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(testString) { [unowned self] (clplacement, error) in
+            guard let placemarks = clplacement, let location = placemarks.first?.location else {
+                print("UNABLE to convert to CLL Coordinates")
+                DispatchQueue.main.async {
+                    completion(false, error)
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.globalLocation = location
+                completion(true, nil)
+            }
+            return
+        }
+    }
+    
+    func handleIsStringToLocationValid(success: Bool, error: Error?){
+        if success {
+            print("GOOD ADDRESS")
+        } else {
+            print("Error when trying to convert string to valid CLLocation = \(String(describing: error?.localizedDescription))")
+            let alertController = UIAlertController(title: "Invalid Entry", message: "Unable to find location on map", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true)
+        }
     }
     
     
+    @objc func handleDeletePLIST(){
+        print("deleting PLIST")
+    }
+
     
     
+    func getMapString()-> String{
+        return locationTextField.text!
+    }
     
+    func getCLLocation()-> CLLocation{
+        return CLLocation()
+    }
     
-    
-    
-    
-    
-    
+    //MARK:- UITextField Delegate Functions
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        textField.placeholder = nil
+//    }
 }
