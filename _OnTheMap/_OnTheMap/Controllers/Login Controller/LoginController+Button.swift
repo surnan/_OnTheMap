1730c62c4c0c91e5e36fa9bc80740e4cc03459f6
Suresh N <forsuresh@yandex.com>
2019-02-12 20:24:06 -0500

    FaceBook button resized to match other UI

--- _OnTheMap/_OnTheMap/Controllers/Login Controller/LoginController+Button.swift
+++ _OnTheMap/_OnTheMap/Controllers/Login Controller/LoginController+Button.swift
@@ -7,7 +7,10 @@
 //
 
 import UIKit
-
+import FacebookCore
+import FacebookLogin
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 extension LoginController{
     
@@ -37,4 +40,44 @@ extension LoginController{
             }
         }
     }
+}
+
+
+let standardButtonHeight: CGFloat = 50
+
+class FacebookButton: FBSDKLoginButton {
+    
+    override func updateConstraints() {
+        // deactivate height constraints added by the facebook sdk (we'll force our own instrinsic height)
+        for contraint in constraints {
+            if contraint.firstAttribute == .height, contraint.constant < standardButtonHeight {
+                // deactivate this constraint
+                contraint.isActive = false
+            }
+        }
+        super.updateConstraints()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: standardButtonHeight)
+    }
+    
+    override func imageRect(forContentRect contentRect: CGRect) -> CGRect {
+        let logoSize: CGFloat = 24.0
+        let centerY = contentRect.midY
+        let y: CGFloat = centerY - (logoSize / 2.0)
+        return CGRect(x: y, y: y, width: logoSize, height: logoSize)
+    }
+    
+    override func titleRect(forContentRect contentRect: CGRect) -> CGRect {
+        if isHidden || bounds.isEmpty {
+            return .zero
+        }
+        
+        let imageRect = self.imageRect(forContentRect: contentRect)
+        let titleX = imageRect.maxX
+        let titleRect = CGRect(x: titleX, y: 0, width: contentRect.width - titleX - titleX, height: contentRect.height)
+        return titleRect
+    }
+    
 }
--- _OnTheMap/_OnTheMap/Controllers/Login Controller/LoginController.swift
+++ _OnTheMap/_OnTheMap/Controllers/Login Controller/LoginController.swift
@@ -8,9 +8,21 @@
 
 import UIKit
 import FacebookCore
-import FacebookLogin
+//import FacebookLogin
+import FBSDKLoginKit
 
-class LoginController: UIViewController {
+//class LoginController: UIViewController, LoginButtonDelegate, FBSDKLoginButtonDelegate {
+    class LoginController: UIViewController, FBSDKLoginButtonDelegate {
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("Test")
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("test 2222")
+    }
+    
+    
+    
     
     lazy var emailTextField: UITextField = {
         let textField = UITextField()
@@ -27,44 +39,22 @@ class LoginController: UIViewController {
         return textField
     }()
     
-    lazy var passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.clearsOnBeginEditing = true
-        textField.defaultTextAttributes = grey25textAttributes
-        textField.attributedText = NSMutableAttributedString(string: "Password", attributes: grey25textAttributes)
-        textField.isSecureTextEntry = true
-        textField.layer.cornerRadius = cornerRadiusSize
-        textField.clipsToBounds = true
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        textField.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
-        return textField
-    }()
+//    lazy var passwordTextField: UITextField = {
+//        let textField = UITextField()
+//        textField.borderStyle = .roundedRect
+//        textField.clearsOnBeginEditing = true
+//        textField.defaultTextAttributes = grey25textAttributes
+//        textField.attributedText = NSMutableAttributedString(string: "Password", attributes: grey25textAttributes)
+//        textField.isSecureTextEntry = true
+//        textField.layer.cornerRadius = cornerRadiusSize
+//        textField.clipsToBounds = true
+//        textField.translatesAutoresizingMaskIntoConstraints = false
+//        textField.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
+//        return textField
+//    }()
     
     
-    lazy var loginButton: UIButton = {  //Need the lazy to have height anchor in definition
-        var button = UIButton()
-        button.backgroundColor = UIColor.steelBlue
-        button.setTitle("Log In", for: .normal)
-        button.layer.cornerRadius = cornerRadiusSize
-        button.clipsToBounds = true
-        button.addTarget(self, action: #selector(handleLoginButton(_:)), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
-        return button
-    }()
-    
-    lazy var facebookButton: UIButton = {
-        var button = UIButton()
-        button.backgroundColor = UIColor.darkBlue
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.setTitle("FACEBOOK", for: .normal)
-        button.layer.cornerRadius = cornerRadiusSize
-        button.clipsToBounds = true
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
-        return button
-    }()
+
     
     var loginStack: UIStackView = {
         let stack = UIStackView()
@@ -75,7 +65,7 @@ class LoginController: UIViewController {
     }()
     
     var logoImage: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.image = #imageLiteral(resourceName: "logo-u")
         imageView.tintColor = UIColor.white
         imageView.contentMode = .scaleAspectFit
@@ -108,74 +98,141 @@ class LoginController: UIViewController {
         return activity
     }()
     
-     let facebookLoginButton = LoginButton(readPermissions: [.publicProfile])
+    lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.clearsOnBeginEditing = true
+        textField.defaultTextAttributes = grey25textAttributes
+        textField.attributedText = NSMutableAttributedString(string: "Password", attributes: grey25textAttributes)
+        textField.isSecureTextEntry = true
+        textField.layer.cornerRadius = cornerRadiusSize
+        textField.clipsToBounds = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
+        return textField
+    }()
     
+    lazy var loginButton: UIButton = {  //Need the lazy to have height anchor in definition
+        var button = UIButton()
+        button.backgroundColor = UIColor.steelBlue
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = cornerRadiusSize
+        button.clipsToBounds = true
+        button.addTarget(self, action: #selector(handleLoginButton(_:)), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
+        return button
+    }()
+    
+
+//    let facebookLoginButton = LoginButton(readPermissions: [.publicProfile])
+    
+//    lazy var facebookLoginButton: LoginButton = {
+//       let button = LoginButton(readPermissions: [.publicProfile])
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.heightAnchor.constraint(equalToConstant: customUIHeightSize).isActive = true
+//        return button
+//    }()
+    
+    
+    
+    
+
+    //    FBSDKLoginManager().logOut()
+    
+//    lazy var myFaceBookLoginButton: UIButton = {
+//       let button = UIButton()
+//        button.backgroundColor = UIColor.blue
+//        button.setAttributedTitle(NSMutableAttributedString(string: "Facebook Login", attributes: white25textAttributes), for: .normal)
+//        button.addTarget(self, action: #selector(handleFacebookLoginButton3), for: .touchUpInside)
+//        return button
+//    }()
+    
+    
+    let facebookFinal = FacebookButton()
+    
+    
+//    @objc func handleFacebookLoginButton3(_ sender: UIButton){
+//
+//        if sender.backgroundColor == UIColor.blue {
+//                sender.setTitle("LOGOUT", for: .normal)
+//
+//        }
+//
+//
+//
+//
+//        let loginManager = LoginManager()
+//        loginManager.logIn(readPermissions: [.publicProfile], viewController: self) { (loginResult) in
+//            switch loginResult {
+//            case .failed(let error):
+//                print(error)
+//            case .cancelled:
+//                print("User cancelled login.")
+//            case .success(_, _, _):
+//                print("Logged in!")
+//            }
+//        }
+//    }
+    
+    let temp = FacebookButton()
     
     
     //MARK:- CODE STARTS HERE
     private func setupUI(){
         emailTextField.text = shazam
         passwordTextField.text = openSesame
+//        facebookLoginButton.translatesAutoresizingMaskIntoConstraints = false
         
-        facebookLoginButton.translatesAutoresizingMaskIntoConstraints = false
-        
-//        [logoImage, loginLabel, emailTextField, passwordTextField, loginButton, facebookButton, facebookLoginButton].forEach{loginStack.addArrangedSubview($0)}
-        
-        [logoImage, loginLabel, emailTextField, passwordTextField, loginButton, facebookLoginButton].forEach{loginStack.addArrangedSubview($0)}
+        [logoImage, loginLabel, emailTextField, passwordTextField, loginButton, temp].forEach{loginStack.addArrangedSubview($0)}
         view.addSubview(loginStack)
+        
+        
         loginLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100).isActive = true
         loginStack.anchor(top: nil, leading: view.leadingAnchor, trailing: view.trailingAnchor, bottom: nil, padding: .init(top: 0, left: 50, bottom: 0, right: 50), size: .zero)
         
         
-        facebookLoginButton.heightAnchor.constraint(equalTo: loginButton.heightAnchor).isActive = true
+        //        facebookLoginButton.heightAnchor.constraint(equalTo: loginButton.heightAnchor).isActive = true
+        
+        checkFacebook()
+        
+        
         
     }
-
     
-
+    func checkFacebook(){
+        if let _ = AccessToken.current  {          //True === Facebook logged in
+            print("Facebook logged in.  Access Token Below:")
+//            print(accessToken)
+        } else {
+            print("Facebook not logged in")
+        }
+    }
+    
+//    func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
+//        switch result {
+//        case .failed(let err):
+//            print("Error after logging in: \n\(err)")
+//        case .cancelled:
+//            print("Login was cancelled")
+//        case .success(_,_,_):
+//            print("success")
+//        }
+//    }
+//
+//    func loginButtonDidLogOut(_ loginButton: LoginButton) {
+//        print("User Logged Out")
+//    }
+    
+    
     //MARK:- Swift Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.white
+//        facebookLoginButton.delegate = self
+        temp.delegate = self
         setupUI()
-        
-        
-        
-        
-        
-        
-        
-//        let loginButton = LoginButton(readPermissions: [.publicProfile])
-//        view.addSubview(loginButton)
-//        loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        loginButton.topAnchor.constraint(equalTo: loginStack.bottomAnchor, constant: 50).isActive = true
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-    
-    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
